4e0ae4d6a0f29dda31af9455de1ccb71230b1df8
JoshScale <joshua.scales@bryanuniversity.edu>
2019-12-18 17:52:53 -0600

    folder structre

---
+++ Week1/groceryCart/grocery.js
@@ -0,0 +1,14 @@
+var shoppingList = {
+    item1:"Apples",
+    item2:"Celery",
+    item3:"chicken",
+    item4:[5,true],
+    item5: function(){
+        return this.item1 + "item2" + item3 
+    }
+ }
+ function shoppingCart(params,another)  {
+     return params + " " + another 
+ }
+console.log(shoppingList)
+console.log(shoppingCart("blue","another"))
