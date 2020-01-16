370443d9aa2a74514e45c1e259af8a4fdcdf04f5
JoshScale <joshua.scales@bryanuniversity.edu>
2020-01-15 21:47:51 -0600

    Updating file

---
+++ week3/computers.js
@@ -0,0 +1,15 @@
+var officeItems = ["stapler","monitor", "computer","desk", "lamp","computer","lamp","stapler","computer","computer"] 
+officeItems.length
+var computer = 0;
+
+for (var i = 0; i< officeItems.length; i++){
+    if(officeItems[i] ==="computer" ) {
+
+        computer++ 
+ 
+ console.log(officeItems[i])
+    }
+}
+
+
+
